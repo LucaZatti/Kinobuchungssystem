b7259b5eb0fb12cb32b882386254609c935177ae
LucaZatti <luca.zatti@bluewin.ch>
2016-05-24 09:42:24 +0200

    add methodes to class Kinosaal

--- Kinobuchungssystem/Kinosaal.cs
+++ Kinobuchungssystem/Kinosaal.cs
@@ -8,6 +8,28 @@ namespace Kinobuchungssystem
 {
     public class Kinosaal
     {
+        private int kinosaalNummer;
+        private int anzahlReihen;
+        private int anzahlPlaetze;
 
+        public Kinosaal()
+        {
+            
+        }
+
+        public void reservierenSaal()
+        {
+
+        }
+
+        public void reservierenPlatz()
+        {
+
+        }
+
+        public void suchenPlatz()
+        {
+
+        }
     }
 }
