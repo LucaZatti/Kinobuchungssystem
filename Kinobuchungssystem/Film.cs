43396545665cff8ad7037779facc3442a9e79858
LucaZatti <luca.zatti@bluewin.ch>
2016-05-24 09:46:05 +0200

    add methode to class Film

--- Kinobuchungssystem/Film.cs
+++ Kinobuchungssystem/Film.cs
@@ -8,6 +8,16 @@ namespace Kinobuchungssystem
 {
     public class Film
     {
+        private string name;
+        private int dauer;
+        private int altersfreigabe;
+        private string produzent;
+        private string genre;
+        private string beschreibung;
 
+        public void verwaltenFilm()
+        {
+
+        }
     }
 }
