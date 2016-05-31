04d1e8d6de777514c991133b582d548a094933f3
LucaZatti <luca.zatti@bluewin.ch>
2016-05-31 11:30:20 +0200

    change Form name

--- Kinobuchungssystem/Form1.Designer.cs
+++ Kinobuchungssystem/Form1.Designer.cs
@@ -34,9 +34,9 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(282, 253);
+            this.ClientSize = new System.Drawing.Size(472, 209);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Kinoverwaltungssystem";
             this.ResumeLayout(false);
 
         }
