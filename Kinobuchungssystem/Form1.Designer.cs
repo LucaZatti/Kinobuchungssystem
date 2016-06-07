fed5b775bc518db9d8c190c542026148e74f38bd
LucaZatti <luca.zatti@bluewin.ch>
2016-06-07 09:20:15 +0200

    add Button and text to form1

--- Kinobuchungssystem/Form1.Designer.cs
+++ Kinobuchungssystem/Form1.Designer.cs
@@ -28,21 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.firstView_btn_Next = new System.Windows.Forms.Button();
+            this.firstView_txt_Einleitung = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // firstView_btn_Next
+            // 
+            this.firstView_btn_Next.Location = new System.Drawing.Point(370, 174);
+            this.firstView_btn_Next.Name = "firstView_btn_Next";
+            this.firstView_btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.firstView_btn_Next.TabIndex = 0;
+            this.firstView_btn_Next.Text = "Next";
+            this.firstView_btn_Next.UseVisualStyleBackColor = true;
+            // 
+            // firstView_txt_Einleitung
+            // 
+            this.firstView_txt_Einleitung.AutoSize = true;
+            this.firstView_txt_Einleitung.Location = new System.Drawing.Point(13, 13);
+            this.firstView_txt_Einleitung.Name = "firstView_txt_Einleitung";
+            this.firstView_txt_Einleitung.Size = new System.Drawing.Size(371, 17);
+            this.firstView_txt_Einleitung.TabIndex = 1;
+            this.firstView_txt_Einleitung.Text = "Herzlich Willkommen zum Kinobuchungssystem der Kitag.\r\n";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(472, 209);
+            this.Controls.Add(this.firstView_txt_Einleitung);
+            this.Controls.Add(this.firstView_btn_Next);
             this.Name = "Form1";
             this.Text = "Kinoverwaltungssystem";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        
+
+        private System.Windows.Forms.Button firstView_btn_Next;
+        private System.Windows.Forms.Label firstView_txt_Einleitung;
     }
 }
 
